8dc32bd113376a7790f0ad273d575c17688601dc
ShilpiAvanish <ShilpiAvanish25@gmail.com>
2024-04-10 20:01:27 -0500

    test commit

--- trader.py
+++ trader.py
@@ -16,6 +16,8 @@ class Trader:
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     
+            # need sto see if commit workging
+            #testing changes that I can potentially make
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:
